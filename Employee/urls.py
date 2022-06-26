5ea69b1ba2963e5e9181bbb055b561c0ecc16466
Sadiqul Islam <mdsadiqulislam390@gmail.com>
2022-06-26 08:47:51 -0700

    employee management system

--- Employee/urls.py
+++ Employee/urls.py
@@ -14,4 +14,12 @@ urlpatterns = [
     path('edit_myexperience/', views.edit_myexperience, name='edit_myexperience'),
     path('my_education/', views.my_education, name='my_education'),
     path('edit_myeducation/', views.edit_myeducation, name='edit_myeducation'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('admin_home/', views.admin_home, name='admin_home'),
+    path('change_passwordadmin/', views.change_passwordadmin, name='change_passwordadmin'),
+    path('all_employee/', views.all_employee, name='all_employee'),
+    path('edit_profile/<int:pid>/', views.edit_profile, name='edit_profile'),
+    path('edit_education/<int:pid>/', views.edit_education, name='edit_education'),
+    path('edit_experience/<int:pid>/', views.edit_experience, name='edit_experience'),
+    path('delete_employee/<int:pid>/', views.delete_employee, name='delete_employee'),
 ]
--- Employee/views.py
+++ Employee/views.py
@@ -96,10 +96,6 @@ def profile(request):
     return render(request, 'profile.html', locals())
 
 
-def admin_login(request):
-    return render(request, 'admin_login.html')
-
-
 def my_experience(request):
     if not request.user.is_authenticated:
         return redirect('emp_login')
@@ -212,3 +208,223 @@ def edit_myeducation(request):
             error = "yes"
 
     return render(request, 'edit_myeducation.html', locals())
+
+
+def change_password(request):
+    if not request.user.is_authenticated:
+        return redirect('emp_login')
+    error = ""
+    user = request.user
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+
+        try:
+            if user.check_password(current_password):
+                user.set_password(new_password)
+                user.save()
+                error = "no"
+            else:
+                error = "not"
+
+        except:
+            error = "yes"
+
+    return render(request, 'change_password.html', locals())
+
+
+
+def admin_login(request):
+    error = ""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        try:
+            if user.is_staff:
+                login(request, user)
+                error = "no"
+            else:
+                error = "yes"
+        except:
+            error = "yes"
+    return render(request, 'admin_login.html', locals())
+
+
+def admin_home(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    return render(request, 'admin_home.html')
+
+
+def change_passwordadmin(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error = ""
+    user = request.user
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+
+        try:
+            if user.check_password(current_password):
+                user.set_password(new_password)
+                user.save()
+                error = "no"
+            else:
+                error = "not"
+
+        except:
+            error = "yes"
+
+    return render(request, 'change_passwordadmin.html', locals())
+
+
+def all_employee(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+
+    employee = EmployeeDetail.objects.all()
+    return render(request, 'all_employee.html', locals())
+
+
+def delete_employee(request,pid):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+
+    user = User.objects.get(id=pid)
+    user.delete()
+    return redirect('all_employee')
+
+
+def edit_profile(request, pid):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error = ""
+
+    employee = EmployeeDetail.objects.get(id=pid)
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        employee_code = request.POST['empcode']
+        department = request.POST['department']
+        designation = request.POST['designation']
+        contact = request.POST['contact']
+        jdate = request.POST['jdate']
+        gender = request.POST['gender']
+
+        employee.user.first_name = first_name
+        employee.user.last_name = last_name
+        employee.empcode = employee_code
+        employee.empdept = department
+        employee.designation = designation
+        employee.contact = contact
+        employee.gender = gender
+        if jdate:
+             employee.joiningdate = jdate
+
+        try:
+            employee.save()
+            employee.user.save()
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'edit_profile.html', locals())
+
+
+def edit_education(request, pid):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error = ""
+
+    user = User.objects.get(id=pid)
+    education = EmployeeEducation.objects.get(user=user)
+    if request.method == 'POST':
+        coursepg = request.POST['coursepg']
+        schoolclgpg = request.POST['schoolclgpg']
+        yearofpassingpg = request.POST['yearofpassingpg']
+        percentagepg = request.POST['percentagepg']
+        coursegra = request.POST['coursegra']
+        schoolclggra = request.POST['schoolclggra']
+        yearofpassinggra = request.POST['yearofpassinggra']
+        percentagegra = request.POST['percentagegra']
+        coursessc = request.POST['coursessc']
+        schoolclgssc = request.POST['schoolclgssc']
+        yearofpassingssc = request.POST['yearofpassingssc']
+        percentagessc = request.POST['percentagessc']
+        coursehsc = request.POST['coursehsc']
+        schoolclghsc = request.POST['schoolclghsc']
+        yearofpassinghsc = request.POST['yearofpassinghsc']
+        percentagehsc = request.POST['percentagehsc']
+
+        education.coursepg = coursepg
+        education.schoolclgpg = schoolclgpg
+        education.yearofpassingpg = yearofpassingpg
+        education.percentagepg = percentagepg
+        education.coursegra = coursegra
+        education.schoolclggra = schoolclggra
+        education.yearofpassinggra = yearofpassinggra
+        education.percentagegra = percentagegra
+        education.coursessc = coursessc
+        education.schoolclgssc = schoolclgssc
+        education.yearofpassingssc = yearofpassingssc
+        education.percentagessc = percentagessc
+        education.coursehsc = coursehsc
+        education.schoolclghsc = schoolclghsc
+        education.yearofpassinghsc = yearofpassinghsc
+        education.percentagehsc = percentagehsc
+
+        try:
+            education.save()
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'edit_education.html', locals())
+
+
+def edit_experience(request, pid):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error = ""
+    user = User.objects.get(id=pid)
+    experience = EmployeeExperience.objects.get(user=user)
+    if request.method == 'POST':
+        company1name = request.POST['company1name']
+        company1desig = request.POST['company1desig']
+        company1salary = request.POST['company1salary']
+        company1duration = request.POST['company1duration']
+        company2name = request.POST['company2name']
+        company2desig = request.POST['company2desig']
+        company2salary = request.POST['company2salary']
+        company2duration = request.POST['company2duration']
+        company3name = request.POST['company3name']
+        company3desig = request.POST['company3desig']
+        company3salary = request.POST['company3salary']
+        company3duration = request.POST['company3duration']
+
+        experience.company1name = company1name
+        experience.company1desig = company1desig
+        experience.company1salary = company1salary
+        experience.company1duration = company1duration
+        experience.company2name = company2name
+        experience.company2desig = company2desig
+        experience.company2salary = company2salary
+        experience.company2duration = company2duration
+        experience.company3name = company3name
+        experience.company3desig = company3desig
+        experience.company3salary = company3salary
+        experience.company3duration = company3duration
+
+        try:
+            experience.save()
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'edit_experience.html', locals())
